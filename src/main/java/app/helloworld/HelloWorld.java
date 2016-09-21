ecc4da871db423293317db197cfdde9b00583d69
GerardoPrada <gpradag@msn.com>
2016-09-21 17:38:59 +0200

    Creates HelloWorld.java

---
+++ src/main/java/app/helloworld/HelloWorld.java
@@ -0,0 +1,21 @@
+package app.helloworld;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class HelloWorld extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/app/helloworld/view/HelloWorld.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) { launch(args); }
+}
